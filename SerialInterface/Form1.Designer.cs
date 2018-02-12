db7ebe9e8189af07ad87ca40f6d63d688922c28b
floathandthing <springtrap.bite87@gmail.com>
2018-02-12 20:15:10 +0000

    Basic structure & interface

--- SerialInterface/Form1.Designer.cs
+++ SerialInterface/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SerialInterface
 {
-    partial class Form1
+    partial class Window
     {
         /// <summary>
         /// Required designer variable.
@@ -29,11 +29,71 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.serialOutput = new System.Windows.Forms.TextBox();
+            this.serialInput = new System.Windows.Forms.TextBox();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.sendButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialOutput
+            // 
+            this.serialOutput.AcceptsReturn = true;
+            this.serialOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.serialOutput.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.serialOutput.Location = new System.Drawing.Point(12, 12);
+            this.serialOutput.Multiline = true;
+            this.serialOutput.Name = "serialOutput";
+            this.serialOutput.ReadOnly = true;
+            this.serialOutput.Size = new System.Drawing.Size(581, 338);
+            this.serialOutput.TabIndex = 0;
+            // 
+            // serialInput
+            // 
+            this.serialInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.serialInput.Location = new System.Drawing.Point(12, 356);
+            this.serialInput.Name = "serialInput";
+            this.serialInput.Size = new System.Drawing.Size(515, 26);
+            this.serialInput.TabIndex = 1;
+            this.serialInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.serialInput_KeyDown);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sendButton.Location = new System.Drawing.Point(533, 356);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(60, 26);
+            this.sendButton.TabIndex = 2;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(605, 410);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.serialInput);
+            this.Controls.Add(this.serialOutput);
+            this.MinimumSize = new System.Drawing.Size(370, 200);
+            this.Name = "Window";
+            this.Text = "Serial Interface";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Window_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox serialOutput;
+        private System.Windows.Forms.TextBox serialInput;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Button sendButton;
     }
 }
 
--- SerialInterface/Form1.cs
+++ SerialInterface/Form1.cs
@@ -10,11 +10,49 @@ using System.Windows.Forms;
 
 namespace SerialInterface
 {
-    public partial class Form1 : Form
+    public partial class Window : Form
     {
-        public Form1()
+        public Window()
         {
             InitializeComponent();
+        }
+
+        private void Window_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void serialInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                //supress the ding sound caused by trying to put an enter in a single line textbox.
+                e.SuppressKeyPress = true;
+
+                //call the sendData void to send the string
+                sendData(serialInput.Text);
+            }
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            sendData(serialInput.Text);
+        }
+
+        void sendData(string data)
+        {
+            //send the string to the serial port if connected.
+            if (serialPort.IsOpen)
+            {
+                serialPort.WriteLine(serialInput.Text);
+
+                //clear the textbox to make way for the next thing they will send.
+                serialInput.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Error sending! Serial port isn't connected.");
+            }
         }
     }
 }
